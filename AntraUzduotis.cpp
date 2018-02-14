025e7b34cca0b36f83cf11bc110402672020afcf
KeepinItReal1 <justas845@gmail.com>
2018-02-15 00:19:23 +0200

    Sortina stack'a

--- AntraUzduotis.cpp
+++ AntraUzduotis.cpp
@@ -5,6 +5,33 @@
 
 using namespace std;
 
+stack<int> sortStack(stack<int> &input)
+{
+    stack<int> tmpStack;
+
+    while (!input.empty())
+    {
+        // pop out the first element
+        int tmp = input.top();
+        input.pop();
+
+        // while temporary stack is not empty and top
+        // of stack is greater than temp
+        while (!tmpStack.empty() && tmpStack.top() > tmp)
+        {
+            // pop from temporary stack and push
+            // it to the input stack
+            input.push(tmpStack.top());
+            tmpStack.pop();
+        }
+
+        // push temp in tempory of stack
+        tmpStack.push(tmp);
+    }
+
+    return tmpStack;
+}
+
 int main(){
 string vardas="";
 string pavarde="";
@@ -38,19 +65,31 @@ if((result>0)&&(result<=10)){
 
 }
 }
+
 cout<<"Jusu ivesti pazymiai:";
 //spausdinimas
-while (!vedimas.empty())
-  {
-     cout << ' ' << vedimas.top();
-     vedimas.pop();
-     skait=skait+1;
-  }
+
+stack<int> tmpStack = sortStack(vedimas);
+
+ while (!tmpStack.empty())
+    {
+        cout << tmpStack.top()<< " ";
+        tmpStack.pop();
+         skait=skait+1;
+    }
+
+
 Vid=VidSum/skait;
 
 float galBalas=0.4*Vid+0.6*egzas;
 cout.precision(3);
 cout<<"\nGalBalas="<<galBalas;
 
+
+while (!vedimas.empty())
+  {
+     cout << ' ' << vedimas.top();
+     vedimas.pop();
+  }
 return 0;
 }
