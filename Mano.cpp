abe5c868020e3c2a909698eb47f01dc98fb92036
KeepinItReal1 <justas845@gmail.com>
2018-03-18 17:13:04 +0200

    Restrukturizacija
    Ko nera pakeista...

---
+++ Mano.cpp
@@ -0,0 +1,123 @@
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdlib.h>
+#include <random>
+#include <time.h>
+#include <functional>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <fstream>
+#include <chrono>
+
+using namespace std;
+
+struct node {
+	string pavarde;
+	string vardas;
+    vector<int> int_vector;
+	int egzas;
+};
+
+int ivedimas=0;
+int skaic=10;//saraso limitas, 10 del grazumo
+node *root= new node[skaic];
+
+
+skaityti(){//skaito duomenis is failo, iraso juos i root
+ifstream inf ( "kursiokai.txt" );
+for(int i=0;i<skaic;i++){
+    int temp=0;
+    inf>>root[i].pavarde >>root[i].vardas;
+    for(int m=0;m<5;m++)
+    {
+    inf>>temp;
+    root[i].int_vector.push_back(temp);
+    }
+   inf >> root[i].egzas;}
+}
+
+spausdinti(){//spausdina root esancius duomenis
+for(int i=0;i<skaic;i++){//SALYGA KOL NERA TUSCIAS
+
+    float Vid=0;int VidSum=0;
+    sort(root[i].int_vector.begin(),root[i].int_vector.end());
+    for(int k=0;k<5;k++){
+        VidSum=VidSum+root[i].int_vector[k];
+    }
+    Vid=static_cast<float>(VidSum)/static_cast<float>(5);
+    float galBalas=0.4*static_cast<float>(Vid) + 0.6*static_cast<float>(root[i].egzas);
+
+    auto mediana=root[i].int_vector[2];//kadangi 5 skaiciai isrikiuoti visalaik bus vidurinis
+
+    if((root[i].pavarde=="")&&(root[i].vardas=="")){break;}
+
+cout<< setw(15) << root[i].pavarde << setw(15) << root[i].vardas;cout<<setw(5)<<setprecision(3)<<galBalas
+<<setw(5)<<setprecision(3)<<mediana<< setw(4) << root[i].egzas <<endl ;
+    }
+}
+
+issaugoti()// iraso i faila esamus root duomenis
+{
+    ofstream myfile ( "kursiokai.txt" );
+    if (myfile.is_open())
+    {
+     for(int i=0;i<ivedimas;i++){//kol nera paskutinis root
+
+    myfile << setw(15) << root[i].pavarde << setw(15) << root[i].vardas ;
+    for(int k=0;k<5;k++)
+    {
+    myfile<< setw(4) << root[i].int_vector[k];
+    }
+    myfile << setw(4) << root[i].egzas <<endl ;
+
+
+
+        }
+        myfile.close();}
+        else cout<<"Cannot open file."<<endl;
+}
+
+ivesti()//ivedimas i root
+{cout<<"Pasirinkite kiek norite kartu ivesti duomenis(nuo 1 iki 10):";
+string ivedimass="";cin>>ivedimass;stringstream(ivedimass)>>ivedimas;cout<<"Jusu pasirinktas kiekis:"<<ivedimas<<endl;
+for(int i=0;i<ivedimas;i++){
+cout<<"Iveskite savo pavarde:";cin>>root[i].pavarde;
+cout<<"Iveskite savo varda:";cin>>root[i].vardas;
+cout<<"Iveskite egzamino pazymi:";
+string egzas1="";cin>>egzas1;stringstream(egzas1)>>root[i].egzas;cout<<endl;
+
+
+cout<<"Jei norite ivertinimus generuoti atsitiktinai spauskite 't'."<<endl;
+string symbol="t";
+string stringas1="";cin>>stringas1;
+
+if(stringas1.compare(symbol)==0)//random generacija
+{
+auto seed = chrono::high_resolution_clock::now().time_since_epoch().count();
+mt19937 mt(seed);
+for(int s=0;s<5;s++){
+uniform_int_distribution<int> skaicius(1,10);
+root[i].int_vector.push_back(skaicius(mt));
+}
+}
+
+else{
+cout<<"Veskite  5 ivertinimus(neteisingi simboliai nebus imami)."<<endl;
+string stringas="";
+for(int h=0;h<5;h++){
+cin>>stringas;
+int result=0;
+stringstream(stringas)>>result;
+if((result>0)&&(result<=10)){
+root[i].int_vector.push_back(result);
+}
+
+}
+}
+}
+}
+
+
--- Mano.h
+++ Mano.h
@@ -23,7 +23,7 @@ struct node {
 	string vardas;
     vector<int> int_vector;
 	int egzas;
-	double vidurkis;
+	double Averag;
 	double median;
 };
 
@@ -38,10 +38,9 @@ vector<node> vargsiukai;//balas maziau uz 60 % FeelsBadMan
 vector<node> kietakai;//balas >= 60 %
 
 
-void skaityti(){//skaito duomenis is failo, iraso juos i root
+void skaityti(){//skaito duomenis is failo, juos issaugo
 ifstream inf;
-inf.exceptions ( ifstream::failbit  );
-try{
+node vnt;
 inf.open(failas);
 string line;
 int i=0;
@@ -51,83 +50,80 @@ while((i<skaic)&&(getline(inf, line))){
     string temp="";
 
     while (S>>temp){elementai.push_back(temp);}//perziureti veliau del exceptions
-
     auto dydis= elementai.size();
-
-    pagr.pavarde=elementai[0];
-    pagr.vardas=elementai[1];
+    vnt.pavarde=elementai[0];
+    vnt.vardas=elementai[1];
     int laikinas;
-    for(int l=0;l<dydis-3;l++){//pirmi du jau yra
-    stringstream(elementai[l+2])>>laikinas;
-    pagr.int_vector.push_back(laikinas);
+    for(int l=2;l<dydis-1;l++){//pirmi du jau yra
+        stringstream(elementai[l])>>laikinas;// max narys dydis-2
+        vnt.int_vector.push_back(laikinas);
     }
     stringstream(elementai[dydis])>>laikinas;
-    pagr.egzas=laikinas;
+    vnt.egzas=laikinas;
+    pagr.push_back(vnt);
     i++;
 }
 inf.close();
-}catch (const ifstream::failure e){
-if (!inf.eof())
-std::cout << "Failed to read from " + failas << std::endl;}
 }
 
 
 void spausdinti(){//spausdina root esancius duomenis
-    for(int i=0;i<skaic;i++){
-        auto dydis= pagr[i].size();
-
-        if(dydis==0){
-            cout<<"Root tuscias."<<endl;
-            std::terminate;
-        }else{
-            if(galBalas<6.0){
-                vargsiukai.push_back(pagr[i]);
-            }
-    else {//galBalas>=6.0
-    kietakai.push_back(pagr[i]);
+int i=0;
+auto dydis= pagr[i].size();
+while(dydis!=0){
+    cout<< setw(15) << pagr[i].pavarde << setw(15) << pagr[i].vardas;cout<<setw(5)<<std::fixed<<setprecision(2)<<galBalas
+    <<setw(5)<<std::fixed<<setprecision(2)<<mediana<< setw(4) << pagr[i].egzas <<endl ;
+    i++;
     }
-if((pagr[i].pavarde=="")&&(pagr[i].vardas=="")){break;}
-
-cout<< setw(15) << pagr[i].pavarde << setw(15) << pagr[i].vardas;cout<<setw(5)<<std::fixed<<setprecision(2)<<galBalas
-<<setw(5)<<std::fixed<<setprecision(2)<<mediana<< setw(4) << pagr[i].egzas <<endl ;
-    }}
 }
 
-void issaugoti(){// iraso i faila esamus root duomenis
-    ofstream myfile (failas);
-    if(myfile.is_open()){
-        for(int i=0;i<ivedimas;i++){
 
-            myfile << setw(15) << pagr[i].pavarde << setw(15) << pagr[i].vardas ;
-            for(int k=0;k<NdIvedimas;k++){
-                myfile<< setw(5) << pagr[i].int_vector[k];
-            }
-            myfile << setw(5) << pagr[i].egzas <<endl ;
-        }
-        myfile.close();
-        }
-    else cout<<"Cannot open file."<<endl;
-}
 
-void ivesti(){//ivedimas i root
+
+void issaugoti(){// sukuria random reiksmes ir iraso jas i faila
+ofstream myfile (failas);
+if(myfile.is_open()){
     auto seed = chrono::high_resolution_clock::now().time_since_epoch().count();
     mt19937 mt(seed);
+    node vnt;
     for(int i=0;i<ivedimas;i++){
         stringstream ss;
         ss<<i;
-        pagr[i].pavarde="vardas"+ss.str();
-
-        pagr[i].vardas="pavarde"+ss.str();
-
+        vnt.pavarde="vardas"+ss.str();
+        vnt.vardas="pavarde"+ss.str();
         uniform_int_distribution<int> skaicius(1,10);
-        pagr[i].egzas=skaicius(mt);
+        vnt.egzas=skaicius(mt);
         for(int s=0;s<NdIvedimas;s++){
-            pagr[i].int_vector.push_back(skaicius(mt));
+            vnt.int_vector.push_back(skaicius(mt));
         }
+        try{
+            vnt.Averag=vidurkis(vnt.int_vector);
+            vnt.median=mediana(vnt.int_vector);
+        }catch (std::domain_error e){cout<<e.what();}
+    }pagr.push_back(vnt);
+
+    if(vnt.Averag<6.0){
+        vargsiukai.push_back(vnt);
     }
+    else {//galBalas>=6.0
+        kietakai.push_back(vnt);
+    }
+
+    for(int i=0;i<ivedimas;i++){
+        myfile << setw(15) << pagr[i].pavarde << setw(15) << pagr[i].vardas ;
+        for(int k=0;k<NdIvedimas;k++){
+            myfile<< setw(5) << pagr[i].int_vector[k];
+        }
+        myfile << setw(5) << pagr[i].egzas <<endl ;
+    }
+    myfile.close();
+    }else cout<<"Cannot open file."<<endl;
 }
 
-static double diffclock(clock_t clock1,clock_t clock2)
+
+
+
+static double diffclock(clock_t clock1,clock_t clock2)//skaiciuoja laika ms tarp clock1 ir clock2
 {
     double diffticks=clock1-clock2;
     double diffms=(diffticks)/(CLOCKS_PER_SEC/1000);
@@ -152,7 +148,6 @@ double vidurkis(vector<int>&V){//grazina vektoriaus vidurki
     return static_cast<float>(VidSum)/static_cast<float>(dydis);
 }
 
-//sulieti ivesti ir issaugoti funkcijas
 #endif // MANO_H
 //float galBalas=0.4*static_cast<float>(Vid) + 0.6*static_cast<float>(pagr.);
 
